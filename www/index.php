cb394c45520486188a08a02a0f07a63109586cfc
tanxc1 <tanxc1@lenovo.com>
2021-04-14 16:20:20 +0800

    docker config

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Tools List";
+?>
+
+<ul>
+	<li>
+		<a href="./tools/phpinfo.php">PHPinfo</a>
+	</li>
+	<li>
+		<a href="./tools/p.php">PHP 探针</a>
+	</li>
+	<li>
+		<a href="./tools/adminer-4.6.3-mysql.php">adminer-4.6.3[数据库管理工具]</a>
+	</li>
+</ul>
