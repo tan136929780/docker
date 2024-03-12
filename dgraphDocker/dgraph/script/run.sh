f54bb6d9403d923f9f5900eaf11eb1cc49bf7fd9
tanxianchen <136929780@qq.com>
2024-03-12 15:39:38 +0800

    增加消息队列

---
+++ dgraphDocker/dgraph/script/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# fail if any error occurs
+set -e
+
+echo -e "\033[0;33m
+Warning: This standalone version is meant for quickstart purposes only.
+         It is NOT RECOMMENDED for production environments.\033[0;0m"
+
+# For Dgraph versions v20.11 and older
+export DGRAPH_ALPHA_WHITELIST=0.0.0.0/0
+# For Dgraph versions v21.03 and newer
+export DGRAPH_ALPHA_SECURITY='whitelist=0.0.0.0/0'
+
+# TODO properly handle SIGTERM for all three processes.
+#dgraph zero & dgraph alpha --cdc "kafka=localhost:9092"
+dgraph zero & dgraph alpha --cdc "file=/dgraph/cdcLog"
